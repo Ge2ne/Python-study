4113a35edd867f4447d31fa323ec3a5e9dce8d65
Ge2ne <43223813+Ge2ne@users.noreply.github.com>
2018-09-13 08:40:21 +0800

    Create args.py

---
+++ Samples/Functions/args.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import math
+
+def product(x, *y):
+    for n in y:
+        x *= n
+    return x
+# 这个应该是最好的一个解决办法：有必选参数，这样如果没输入会报错；有可变参数，这样得到一个tuple
+# 并且最终回归值是x，省了行数。
+print('product(5) =', product(5))
+print('product(5, 6) =', product(5, 6))
+print('product(5, 6, 7) =', product(5, 6, 7))
+print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
+if product(5) != 5:
+    print('测试失败！')
+elif product(5,6) != 30:
+    print('测试失败！')
+elif product(5, 6, 7) != 210:
+    print('测试失败!')
+elif product(5, 6, 7, 9) != 1890:
+    print('测试失败!')
+else:
+    try:
+        product()
+        print('测试失败!')
+    except TypeError:
+        print('测试成功!')
