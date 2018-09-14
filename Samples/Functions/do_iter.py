27776ac65a709dbb155215af2ad7896978c5d7db
Ge2ne <43223813+Ge2ne@users.noreply.github.com>
2018-09-14 22:26:29 +0800

    Update do_iter.py

--- Samples/Functions/do_iter.py
+++ Samples/Functions/do_iter.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 
 def findMinAndMax(L):
-	if L == []:
-		return (None, None)
-	else:
-		min = L[0]
-		max = L[0]
-		for x in L:
-			if x < min:
-				min = x
-			if x > max:
-				max = x
+	if L:
+		min=L[0]
+		max=L[0]
+		for value in L:
+			if min>=value:
+				min=value
+			if max<=value:
+				max=value
 		return (min, max)
+	else:
+		return (None,None)
 
 # 测试
 if findMinAndMax([]) != (None, None):
